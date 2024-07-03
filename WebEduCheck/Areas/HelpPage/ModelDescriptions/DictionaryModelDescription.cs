7e8a2748665eb9fe761a468b7386a9903c8995d2
David songa <songadavid06@gmail.com>
2024-07-03 13:30:01 +0200

    Initial commit

---
+++ WebEduCheck/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebEduCheck.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ WebEduCheck/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebEduCheck.MvcApplication" Language="C#" %>
